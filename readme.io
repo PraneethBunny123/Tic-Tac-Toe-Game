1da034942f7defc0addd463e7b51cbacfe3d6164
praneeth <gp.gunturu@gmail.com>
2024-12-30 15:54:57 -0600

    first commit of tic-tac-toe game

---
+++ readme.io
@@ -0,0 +1,2 @@
+1. If no node modules do 'npm install' to install dependencies
+2. Next do 'npm run dev'
---
+++ src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react"
+
+export default function Player ({initialName, symbol, isActive, onChangeName}) {
+
+    const [playerName, setPlayerName] = useState(initialName)
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleEditClick() {
+        setIsEditing(editing => !editing)
+
+        if(isEditing) {
+            onChangeName(symbol, playerName)
+        }
+
+    }
+
+    function handleChange(e) {
+        setPlayerName(e.target.value)
+    }
+
+    let editablePlayerName = (<span className="player-name">{playerName}</span>)
+    if(isEditing) {
+        editablePlayerName = (<input 
+            className="player-name" 
+            type='text' 
+            required 
+            value={playerName} 
+            placeholder="Enter name"
+            onChange={handleChange}
+        />)
+    }
+
+    return (
+        <li className={isActive ? 'active' : undefined}>
+            <span id="player">
+                {editablePlayerName}
+                <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+        </li>
+    )
+}
